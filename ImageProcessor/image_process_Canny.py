32d49d708d352965982f627e67275aeede8ef0b0
ItsukiMatsunaga <69783579+ItsukiMatsunaga@users.noreply.github.com>
2020-12-03 15:50:36 +0900

    追加おっけです

--- ImageProcessor/image_process_Canny.py
+++ ImageProcessor/image_process_Canny.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 def image_process_Canny(): #  引数として画像を追加する
-    org_img = cv2.imread('/Users/k19109kk/src/Git/Web_Image/ImageProcessor/uchitane_far.png') #  追加した引数で読み込む
+    org_img = cv2.imread('/Users/k19100kk/Documents/GitHub/Web_Image/ImageProcessor/uchitane_far.png') #  追加した引数で読み込む
     # print(org_img)  #  ファイル読み込みの確認用
 
     edge_img = cv2.Canny(org_img,100,200) #  Canny法で加工した画像を保存
