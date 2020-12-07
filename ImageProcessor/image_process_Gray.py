178d39cacd3ea33797e0dd14a8354126d8bcec5b
Ayaha Morinaga <ayaha.morinaga@gmail.com>
2020-12-08 08:17:36 +0900

    森永 入力画像ディレクトリにある画像数と出力画像ディレクトリにある画像数が等しくなるように修正
    入力画像ディレクトリにある画像数と出力画像ディレクトリにある画像数が等しくなるように修正しました。それと関数を作ってコードを見やすくしました。

--- ImageProcessor/image_process_Gray.py
+++ ImageProcessor/image_process_Gray.py
@@ -5,7 +5,10 @@ import cv2
 
 OUT_DIR = "image_gray/"
 
-def ready():
+"""
+関数説明：入力画像ディレクトリにある画像を全てグレースケール化する
+"""
+def Turn_image_add_into_Gray():
     #入力画像ディレクトリにあるファイル名のリスト
     add_files = os.listdir("image_add")
     #出力画像ディレクトリにあるファイル名のリスト
@@ -20,7 +23,11 @@ def ready():
         gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         #画像保存
         cv2.imwrite(OUT_DIR + notgray_file,gray)
-    
+
+"""
+関数説明：出力画像ディレクトリにある画像を全てグレースケール化する
+"""
+def Turn_image_gray_into_Gray():   
     #出力画像ディレクトリにあるファイル名のリスト
     gray_files = os.listdir("image_gray")
     #全てグレースケール化
@@ -34,10 +41,27 @@ def ready():
         #画像保存
         cv2.imwrite(OUT_DIR + gray_file,gray)
 
+"""
+関数説明：入力画像ディレクトリにある画像の数と出力画像ディレクトリにある画像の数を等しくする関数
+"""
+def Adjust_the_number_of_files():
+    #入力画像ディレクトリにあるファイル名のリスト
+    add_files = os.listdir("image_add")
+    #出力画像ディレクトリにあるファイル名のリスト
+    gray_files = os.listdir("image_gray")
+    #出力画像ディレクトリから入力画像ディレクトリに存在しないファイル名を削除
+    none_exit_in_add_files = [i for i in gray_files if not i in add_files]
+    for none_exit_in_add_file in none_exit_in_add_files:
+        #上記で読み込んだファイルを削除
+        os.remove(OUT_DIR + none_exit_in_add_file)
+    
+
 
 if __name__ in '__main__':
     # 処理が終了しないようにスリープを挟んで無限ループ
     while True:
         # 1秒ごとに監視
         time.sleep(1)
-        ready()
+        Turn_image_add_into_Gray()
+        Turn_image_gray_into_Gray()
+        Adjust_the_number_of_files()
