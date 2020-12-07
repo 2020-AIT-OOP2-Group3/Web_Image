15d6f56dd3205f762ca60d47fe839bb25013dd39
KUWABARA0217 <75057525+KUWABARA0217@users.noreply.github.com>
2020-12-07 22:52:42 +0900

    桑原の変更

--- ImageProcessor/image_process_Kao.py
+++ ImageProcessor/image_process_Kao.py
@@ -1,42 +1,41 @@
-from watchdog.events import FileSystemEventHandler
-from watchdog.observers import Observer
 import cv2
 import time
+import os
+import sys
 
+# スクリプト実行用ディレクトリを絶対パスで指定する
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+OUT_DIR = "image_face/"
 
-class ChangeHandler(FileSystemEventHandler):
+def ready():
+    #image_addディレクトリにあるファイル名のリスト
+    add_files = os.listdir("image_add")
+    #image_faceディレクトリにあるファイル名のリスト
+    face_files = os.listdir("image_face")
+    #image_addにある画像の中で画像処理されてない画像を見つける
+    notface_files = [i for i in add_files if not i in face_files]
+    #全て画像処理
+    for notface_file in notface_files:
+        #画像を読み込む
+        face = cv2.imread("image_add/" + notface_file)
+        #顔認識処理
+        gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+        cascade_path = BASEDIR + "/haarcascade_frontalface_default.xml"
+        cascade = cv2.CascadeClassifier(cascade_path) 
+        facerect = cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=4, minSize=(100, 100))
+        
+        rectangle_color = (0, 0, 255)
     
-    def on_created(self, event):
-        addfile_name=str(event)
-        addfile_name=addfile_name.replace('<FileCreatedEvent: src_path=','')
-        addfile_name=addfile_name.replace(">",'')
-        addfile_name=addfile_name.replace("'",'')
-        addfile_name=addfile_name.replace("'",'')
-
-        image_process_Kao(addfile_name)
-
-def image_process_Kao(file_name):
-    cascade_path = "/Users/k19100kk/Documents/GitHub/Web_Image/ImageProcessor/haarcascade_frontalface_default.xml"
-    face_img = cv2.imread(file_name) 
-    gry_img = cv2.cvtColor(face_img, cv2.COLOR_BGR2GRAY)
+        if len(facerect) > 0:
+            for rect in facerect:
+                cv2.rectangle(face, tuple(rect[0:2]),tuple(rect[0:2] + rect[2:4]), rectangle_color, thickness=2)
     
-    cascade = cv2.CascadeClassifier(cascade_path) 
-    facerect = cascade.detectMultiScale(gry_img, scaleFactor=1.05, minNeighbors=4, minSize=(100, 100))
-    
-    rectangle_color = (0, 0, 255)
-    
-    if len(facerect) > 0:
-        for rect in facerect:
-            cv2.rectangle(face_img, tuple(rect[0:2]),tuple(rect[0:2] + rect[2:4]), rectangle_color, thickness=2)
+        #画像保存
+        cv2.imwrite(OUT_DIR + notface_file,face)
 
-    cv2.imwrite('/Users/k19100kk/Documents/GitHub/Web_Image/ImageProcessor/image_face/face_img.png',face_img)
-    
-
-observer = Observer()
-# 監視するフォルダを第２引数に指定
-observer.schedule(ChangeHandler(), '/Users/k19100kk/Documents/GitHub/Web_Image/ImageProcessor/image_add', recursive=True)
-# 監視を開始する
-observer.start()
-
-while True:
-    time.sleep(5)
+if __name__ in '__main__':
+    # 処理が終了しないようにスリープを挟んで無限ループ
+    while True:
+        # 1秒ごとに監視
+        time.sleep(1)
+        ready()
--- ImageProcessor/image_process_Mozaiku.py
+++ ImageProcessor/image_process_Mozaiku.py
@@ -1,43 +1,43 @@
-from watchdog.events import FileSystemEventHandler
-from watchdog.observers import Observer
 import cv2
 import time
+import os
+import sys
 
-class ChangeHandler(FileSystemEventHandler):
+# スクリプト実行用ディレクトリを絶対パスで指定する
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+OUT_DIR = "image_mozaiku/"
+
+def ready():
+    #image_addディレクトリにあるファイル名のリスト
+    add_files = os.listdir("image_add")
+    #image_mozaikuディレクトリにあるファイル名のリスト
+    mozaiku_files = os.listdir("image_mozaiku")
+    #image_addにある画像の中で画像処理されてない画像を見つける
+    notmozaiku_files = [i for i in add_files if not i in mozaiku_files]
+    #全て画像処理
+    for notmozaiku_file in notmozaiku_files:
+        #画像を読み込む
+        mozaiku = cv2.imread("image_add/" + notmozaiku_file)
+        #顔認識処理
+        gray = cv2.cvtColor(mozaiku, cv2.COLOR_BGR2GRAY)
+        cascade_path = BASEDIR + "/haarcascade_frontalface_default.xml"
+        cascade = cv2.CascadeClassifier(cascade_path) 
+        facerect = cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=4, minSize=(100, 100))
+        
+        if len(facerect) > 0:
+            for face in facerect:
+                x, y, w, h = face
+                roi = mozaiku[y:y+h, x:x+w]
+                roi = cv2.resize(roi, (w//10, h//10))
+                roi = cv2.resize(roi, (w, h), interpolation=cv2.INTER_NEAREST)
+                mozaiku[y:y+h, x:x+w] = roi          
     
-    def on_created(self, event):
-        addfile_name=str(event)
-        addfile_name=addfile_name.replace('<FileCreatedEvent: src_path=','')
-        addfile_name=addfile_name.replace(">",'')
-        addfile_name=addfile_name.replace("'",'')
-        addfile_name=addfile_name.replace("'",'')
+        #画像保存
+        cv2.imwrite(OUT_DIR + notmozaiku_file,mozaiku)
 
-        image_process_Mozaiku(addfile_name)
-
-def image_process_Mozaiku(file_name):
-    cascade_path = "/Users/k19100kk/Documents/GitHub/Web_Image/ImageProcessor/haarcascade_frontalface_default.xml"
-
-    mozaiku_img = cv2.imread(file_name) 
-    gry_img = cv2.cvtColor(mozaiku_img,cv2.COLOR_BGR2GRAY)
-
-    cascade = cv2.CascadeClassifier(cascade_path)
-    faces = cascade.detectMultiScale(gry_img, scaleFactor=1.05, minNeighbors=4, minSize=(100, 100))
-
-    if len(faces) > 0:
-        for face in faces:
-            x, y, w, h = face
-            roi = mozaiku_img[y:y+h, x:x+w]
-            roi = cv2.resize(roi, (w//10, h//10))
-            roi = cv2.resize(roi, (w, h), interpolation=cv2.INTER_NEAREST)
-            mozaiku_img[y:y+h, x:x+w] = roi               
-
-    cv2.imwrite('/Users/k19100kk/Documents/GitHub/Web_Image/ImageProcessor/image_mozaiku/mozaiku_img.png',mozaiku_img)
-    
-observer = Observer()
-# 監視するフォルダを第２引数に指定
-observer.schedule(ChangeHandler(), '/Users/k19100kk/Documents/GitHub/Web_Image/ImageProcessor/image_add', recursive=True)
-# 監視を開始する
-observer.start()
-
-while True:
-    time.sleep(5)
+if __name__ in '__main__':
+    # 処理が終了しないようにスリープを挟んで無限ループ
+    while True:
+        # 1秒ごとに監視
+        time.sleep(1)
+        ready()
