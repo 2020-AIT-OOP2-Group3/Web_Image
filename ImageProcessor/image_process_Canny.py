878086992532fb5a0a8fe1bd724ac423997a5f40
ItsukiMatsunaga <69783579+ItsukiMatsunaga@users.noreply.github.com>
2020-12-08 12:15:21 +0900

    Carryと削除されたファイルを追加しました

--- ImageProcessor/image_process_Canny.py
+++ ImageProcessor/image_process_Canny.py
@@ -1,33 +1,55 @@
-from watchdog.events import FileSystemEventHandler
-from watchdog.observers import Observer
+import os
 import cv2
 import time
-import numpy as np
+import sys
 
-class ChangeHandler(FileSystemEventHandler):
+OUT_DIR = "image_carry/"
 
-    def on_created(self, event):
-        addfile_name=str(event)
-        addfile_name=addfile_name.replace('<FileCreatedEvent: src_path=','')
-        addfile_name=addfile_name.replace(">",'')
-        addfile_name=addfile_name.replace("'",'')
-        addfile_name=addfile_name.replace("'",'')
+'''
+関数説明：入力画像ディレクトリにある画像を全てcarry法で加工
+'''
 
-        image_process_Canny(addfile_name)
+def Turn_image_add_into_carry():
+    path = './image_add'
+    add_files = os.listdir(path)
+    carry_files = os.listdir("image_carry/")
+    notcarry_files = [i for i in add_files if not i in carry_files]
+    for notcarry_files in notcarry_files:
+        img = cv2.imread("image_add/" + notcarry_files)
+        carry = cv2.Canny(img,100,200)
+        cv2.imwrite(OUT_DIR + notcarry_files,carry)
 
-def image_process_Canny(file_name): #  引数として画像を追加する
-    cascade_path = "/Users/k19100kk/Documents/GitHub/Web_Image/ImageProcessor/haarcascade_frontalface_default.xml"
-    org_img = cv2.imread(file_name) # 追加した引数で読み込む
-    # print(org_img)  #  ファイル読み込みの確認用
+'''
+関数説明：出力画像ディレクトリにある画像を全てcarry法で加工
+'''
 
-    edge_img = cv2.Canny(org_img,100,200) #  Canny法で加工した画像を保存
-    cv2.imwrite('/Users/k19100kk/Documents/GitHub/Web_Image/ImageProcessor/image_face/face_img.png',edge_img)
+def Turn_image_carry_into_carry():
+    carry_files = os.listdir("image_carry")
+    for carry_files in carry_files:
+        img2 = cv2.imread(OUT_DIR + carry_files)
+        os.remove(OUT_DIR + carry_files)
+        carry = cv2.Canny(img2,100,200)
+        cv2.imwrite(OUT_DIR + carry_files,carry)
 
-observer = Observer()
-# 監視するフォルダを第２引数に指定
-observer.schedule(ChangeHandler(), '/Users/k19100kk/Documents/GitHub/Web_Image/ImageProcessor/image_add', recursive=True)
-# 監視を開始する
-observer.start()
+"""
+関数説明：入力画像ディレクトリにある画像の数と出力画像ディレクトリにある画像の数を等しくする関数
+"""
+def Adjust_the_number_of_files():
+    #入力画像ディレクトリにあるファイル名のリスト
+    add_files = os.listdir("image_add")
+    #出力画像ディレクトリにあるファイル名のリスト
+    carry_files = os.listdir("image_carry")
+    #出力画像ディレクトリから入力画像ディレクトリに存在しないファイル名を削除
+    none_exit_in_add_files = [i for i in carry_files if not i in add_files]
+    for none_exit_in_add_file in none_exit_in_add_files:
+        #上記で読み込んだファイルを削除
+        os.remove(OUT_DIR + none_exit_in_add_file)
 
-while True:
-    time.sleep(5)
+if __name__ in '__main__':
+    # 処理が終了しないようにスリープを挟んで無限ループ
+    while True:
+        # 1秒ごとに監視
+        time.sleep(1)
+        Turn_image_add_into_carry()
+        Turn_image_carry_into_carry()
+        Adjust_the_number_of_files()
