7d430921b7201fa868ddc43a81cb15b4329db221
KUWABARA0217 <75057525+KUWABARA0217@users.noreply.github.com>
2020-12-08 10:59:47 +0900

    桑原の変更２

--- ImageProcessor/image_process_Mozaiku.py
+++ ImageProcessor/image_process_Mozaiku.py
@@ -7,7 +7,8 @@ import sys
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 OUT_DIR = "image_mozaiku/"
 
-def ready():
+#入力画像ディレクトリにある画像を処理
+def Turn_image_add_into_Mozaiku():
     #image_addディレクトリにあるファイル名のリスト
     add_files = os.listdir("image_add")
     #image_mozaikuディレクトリにあるファイル名のリスト
@@ -35,9 +36,42 @@ def ready():
         #画像保存
         cv2.imwrite(OUT_DIR + notmozaiku_file,mozaiku)
 
+#出力画像ディレクトリにある画像を処理
+def Turn_image_mozaiku_into_Mozaiku():
+    mozaiku_files = os.listdir("image_mozaiku")
+    for mozaiku_file in mozaiku_files:
+        #画像を読み込む
+        mozaiku2 = cv2.imread(OUT_DIR + mozaiku_file)
+        os.remove(OUT_DIR + mozaiku_file)
+        #顔認識処理
+        gray = cv2.cvtColor(mozaiku2, cv2.COLOR_BGR2GRAY)
+        cascade_path = BASEDIR + "/haarcascade_frontalface_default.xml"
+        cascade = cv2.CascadeClassifier(cascade_path) 
+        facerect = cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=4, minSize=(100, 100))
+        
+        if len(facerect) > 0:
+            for face in facerect:
+                x, y, w, h = face
+                roi = mozaiku2[y:y+h, x:x+w]
+                roi = cv2.resize(roi, (w//10, h//10))
+                roi = cv2.resize(roi, (w, h), interpolation=cv2.INTER_NEAREST)
+                mozaiku2[y:y+h, x:x+w] = roi         
+        #画像保存
+        cv2.imwrite(OUT_DIR + mozaiku_file,mozaiku2)
+
+#入力、出力ディレクトリにある画像数を揃える
+def Adjust_the_number_of_files():
+    adds_files = os.listdir("image_add")
+    mozaiku_files = os.listdir("image_mozaiku")
+    none_exit_in_add_files = [i for i in mozaiku_files if not i in adds_files]
+    for none_exit_in_add_file in none_exit_in_add_files:
+        os.remove(OUT_DIR + none_exit_in_add_file)
+
 if __name__ in '__main__':
     # 処理が終了しないようにスリープを挟んで無限ループ
     while True:
         # 1秒ごとに監視
         time.sleep(1)
-        ready()
+        Turn_image_add_into_Mozaiku()
+        Turn_image_mozaiku_into_Mozaiku()
+        Adjust_the_number_of_files()
